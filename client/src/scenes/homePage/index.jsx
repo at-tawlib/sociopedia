ec0bd8663847f2314a452fc4326bb426cf68a40e
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-12-09 08:25:09 +0000

    Finish Navbar

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,11 @@
+import Navbar from "scenes/navbar";
+import { Box } from "@mui/material";
+
+const HomePage = () => {
+    return <Box>
+        <Navbar />
+        Yo
+    </Box>
+};
+
+export default HomePage;
