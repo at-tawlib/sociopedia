0698c4f5e7923af37310ec09ec827a9dacb351e6
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-12-10 08:21:46 +0000

    Add Login and register form

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,35 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+import Form2 from "./form2";
+
 const LoginPage = () => {
-    return (<div>Login page</div>);
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    return <Box>
+        <Box
+            width="100%"
+            backgroundColor={theme.palette.alt}
+            p="1rem 6%"
+            textAlign="center" >
+            <Typography fontWeight="bold" fontSize="32px" color="primary">
+                Sociopedia
+            </Typography>
+        </Box>
+        <Box
+            width={isNonMobileScreens ? "50%" : "93%"}
+            p="2rem"
+            m="2rem auto"
+            borderRadius="1.5rem"
+            backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+                Welcome to Sociopedia, the Social Media for Sociopaths!
+            </Typography>
+            <Form />
+            {/* <Form2 /> */}
+        </Box>
+    </Box>;
 };
 
 export default LoginPage;
