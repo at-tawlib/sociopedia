849b8845ebfa58581ef7c1bd8503531fc1ec1b8e
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-12-11 04:12:32 +0000

    Fixed bug of user router error

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
+app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
